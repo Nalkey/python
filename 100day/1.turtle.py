d12c531d047ed2732b4d393e6133879af126c2ef
Yuanhao WU <yuanhao_wu@yeah.net>
2021-01-30 21:05:25 +0800

    Day1 Turtle

---
+++ 100day/1.turtle.py
@@ -0,0 +1,30 @@
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('blue')
+turtle.speed(2)
+
+turtle.goto(-100,100)
+turtle.right(45)
+turtle.forward(100)
+turtle.up()
+turtle.goto(100,100)
+turtle.right(90)
+turtle.down()
+turtle.forward(141)
+turtle.left(45)
+turtle.forward(100)
+
+turtle.mainloop()
+#import turtle
+#
+#turtle=turtle.Turtle()
+#screen=turtle.getscreen()
+#
+#turtle.color('red', 'yellow')
+#turtle.begin_fill()
+#for i in range(50):
+#    turtle.forward(200)
+#    turtle.left(170)
+#turtle.end_fill()
+#screen.mainloop()
